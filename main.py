0b82ffdca85467fa83fa2ba0bed7e69c9f6266c4
VladyslavKuksa <student.vlad.kuksa@gmail.com>
2023-06-04 15:15:59 +0300

    1commit

--- main.py
+++ main.py
@@ -1,16 +1,110 @@
-# This is a sample Python script.
+import pygame
+import random
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Константи
+WIDTH = 800
+HEIGHT = 600
+ROWS = 10
+COLS = 10
+CELL_SIZE = 50
+FPS = 30
+MINE_COUNT = 10
 
+# Кольори
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Ініціалізація Pygame
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
 
+# Клас комірки
+class Cell:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.x = col * CELL_SIZE
+        self.y = row * CELL_SIZE
+        self.mine = False
+        self.flagged = False
+        self.revealed = False
+        self.adjacent_mines = 0
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def draw(self):
+        if self.revealed:
+            pygame.draw.rect(screen, GRAY, (self.x, self.y, CELL_SIZE, CELL_SIZE))
+            if self.mine:
+                pygame.draw.circle(screen, BLACK, (self.x + CELL_SIZE // 2, self.y + CELL_SIZE // 2), CELL_SIZE // 4)
+            elif self.adjacent_mines > 0:
+                font = pygame.font.Font(None, CELL_SIZE // 2)
+                text = font.render(str(self.adjacent_mines), True, BLACK)
+                text_rect = text.get_rect(center=(self.x + CELL_SIZE // 2, self.y + CELL_SIZE // 2))
+                screen.blit(text, text_rect)
+        else:
+            pygame.draw.rect(screen, WHITE, (self.x, self.y, CELL_SIZE, CELL_SIZE))
+            if self.flagged:
+                font = pygame.font.Font(None, CELL_SIZE // 2)
+                text = font.render("F", True, BLACK)
+                text_rect = text.get_rect(center=(self.x + CELL_SIZE // 2, self.y + CELL_SIZE // 2))
+                screen.blit(text, text_rect)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Генерація дошки
+board = []
+for row in range(ROWS):
+    board.append([])
+    for col in range(COLS):
+        board[row].append(Cell(row, col))
+
+# Розміщення мін
+mines = random.sample(range(ROWS * COLS), MINE_COUNT)
+for mine in mines:
+    row = mine // COLS
+    col = mine % COLS
+    board[row][col].mine = True
+
+# Підрахунок кількості сусідніх мін для кожної комірки
+for row in range(ROWS):
+    for col in range(COLS):
+        if not board[row][col].mine:
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if 0 <= row + i < ROWS and 0 <= col + j < COLS:
+                        if board[row + i][col + j].mine:
+                            board[row][col].adjacent_mines += 1
+
+# Головний цикл гри
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                # Обробка лівого кліку миші
+                pos = pygame.mouse.get_pos()
+                col = pos[0] // CELL_SIZE
+                row = pos[1] // CELL_SIZE
+                if not board[row][col].flagged:
+                    board[row][col].revealed = True
+                    if board[row][col].mine:
+                        # Гравець програв
+                        running = False
+            elif event.button == 3:
+                # Обробка правого кліку миші
+                pos = pygame.mouse.get_pos()
+                col = pos[0] // CELL_SIZE
+                row = pos[1] // CELL_SIZE
+                if not board[row][col].revealed:
+                    board[row][col].flagged = not board[row][col].flagged
+
+    # Оновлення екрану
+    screen.fill(BLACK)
+    for row in range(ROWS):
+        for col in range(COLS):
+            board[row][col].draw()
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
